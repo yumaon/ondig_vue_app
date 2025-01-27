66a9344f6e5bba94f64ffd45bdb1963482689a98
yumaon <110388364+yumaon@users.noreply.github.com>
2025-01-27 21:29:57 +0900

    VueUseの導入

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,30 +1,42 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+import { useAnimate } from "@vueuse/core";
+import { ref, onMounted } from "vue";
+
+const el = ref(null);
+const { animate } = useAnimate(
+  el,
+  {
+    opacity: [0, 1],
+    transform: ["translateY(20px)", "translateY(0)"],
+  },
+  {
+    duration: 1000,
+    delay: 100,
+    easing: "ease-out",
+  }
+);
+
+onMounted(() => {
+  animate();
+});
 </script>
 
 <template>
-    <Head title="Dashboard" />
+  <Head title="Dashboard" />
 
-    <AuthenticatedLayout>
-        <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
-                Dashboard
-            </h2>
-        </template>
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="text-xl font-semibold leading-tight text-gray-800">Dashboard</h2>
+    </template>
 
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
-                    <div class="p-6 text-gray-900">
-                        You're logged in!
-                    </div>
-                </div>
-            </div>
+    <div class="py-12">
+      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+          <div ref="el" class="p-6 text-gray-900">こんにちは！</div>
         </div>
-    </AuthenticatedLayout>
+      </div>
+    </div>
+  </AuthenticatedLayout>
 </template>
